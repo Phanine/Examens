c35b70a3a8b451a8e87fa3f1d841f7d53e9371e0
Phanine <113602934+Phanine@users.noreply.github.com>
2022-11-06 19:33:12 +0000

    Instruc+solut

--- wdmatch.go
+++ wdmatch.go
@@ -1,3 +1,10 @@
+Instructions
+Write a program that takes two string and checks whether it is possible to write the first string with characters from the second string. This rewrite must respect the order in which these characters appear in the second string.
+
+If it is possible, the program displays the string followed by a newline ('\n'), otherwise it simply displays nothing.
+
+If the number of arguments is different from 2, the program displays nothing.
+
 package main
 
 import (
